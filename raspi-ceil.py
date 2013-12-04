e0ce511ca1f61011601f175ecc2b70aa1e9ad94c
jsyoung <joeyoungwx@gmail.com>
2013-12-04 17:47:05 -0600

    found a variable name conlict

--- raspi-ceil.py
+++ raspi-ceil.py
@@ -3,7 +3,7 @@
 import serial
 import time
 import datetime
-import logging as l
+import logging as lg
 import os
 import sys
 
@@ -61,113 +61,115 @@ def save(data):
 
 
 def main(BAUDRATE, BYTESIZE, BOM, EOM, PORT, FILESTR, LOCATION, DELAY, devmode=False):
-	"""
-	The main loop for listening to a Vaisala ceilometer connected via USB
-	
-	"""
+    """
+    The main loop for listening to a Vaisala ceilometer connected via USB
+    
+    """
 
-	logfilename = LOCATION + "log/raspi-ceil.log"
-	if devmode:
-	    logfilename = None
-    l.basicConfig(filename=logfilename, filemode='a',
-              format="%(asctime)s %(levelname)s: %(message)s", level=l.DEBUG)
-
-
-
-
-	# this block of code checks if there is already a process
-	# running that is performing this task
-	if os.path.exists(LOCATION + ".raspiceilpid"):
-	    # check it
-	    f = open(LOCATION + "/.raspiceilpid", 'r')
-	    pid = f.read()
-	    f.close()
-	    try:
-	        #  os.kill sends a signal to a process, signal 0 ilicits no response, so this does not actually kill anything
-	        os.kill(int(pid), 0)
-	        print "Another listener is already running"
-	        if not devmode:
-	            exit()
-	        else:
-	            import signal
-	            print 'DEVMODE: killing current process'
-	            os.kill(int(pid), signal.SIGTERM)  # or SIGKILL or SIGABORT...
-	    except OSError:
-	        pass
-	    # ok, that process is not running, so continue
-
-	else:
-	    # ok, well, that's the best I can do, continue
-	    pass
-
-	f = open(LOCATION + "/.raspiceilpid", 'w')
-	f.write(str(os.getpid()))
-	f.close()
-	l.info('Beginning active data collection')
-	if devmode:
-	    print "DEVMODE: no data being collected, all data printing to screen"
-
-	ser = serial.Serial()
-	ser.baudrate = BAUDRATE
-	ser.bytesize = BYTESIZE
+    logfilename = LOCATION + "log/raspi-ceil.log"
+    if devmode:
+        logfilename = None
+    lg.basicConfig(filename=logfilename, filemode='a',
+                  format="%(asctime)s %(levelname)s: %(message)s", level=lg.DEBUG)
 
 
 
-	# FIXME - UTILIZE THE LIST_PORTS.COMPORTS() TO GET IDS!
-	# #ser.timeout=1
-	port = PORT
-	while port < 20:
-	    # loop through all likely ports...
-	    try:
-	        ser.port = '/dev/ttyUSB%i' % port
-	        ser.open()
-	        break  # try no more!
-	    except serial.serialutil.SerialException as e:
-	        print e
-	        port += 1
-	if port == 20:
-	    l.warning('No valid USB serial port identified, aborting')
-	    exit()
 
 
-	l.info("Connected to a USB device on {}".format(ser.port))
 
-	print """
-	
-		DEVMODE: PRINTING SERIAL DATA RECEIVED. 
-		NOTE: GARBLED TEXT MEANS BAUD RATE AND/OR BYTE SIZE NOT ACCURATELY SPECIFIED 
-	
-	/////////////////////////////////////////////////////////////////////////////////
-		
-	"""
+    # this block of code checks if there is already a process
+    # running that is performing this task
+    if os.path.exists(LOCATION + ".raspiceilpid"):
+        # check it
+        f = open(LOCATION + ".raspiceilpid", 'r')
+        pid = f.read()
+        f.close()
+        try:
+            #  os.kill sends a signal to a process, signal 0 ilicits no response, so this does not actually kill anything
+            os.kill(int(pid), 0)
+            print "Another listener is already running"
+            if not devmode:
+                exit()
+            else:
+                import signal
+                print 'DEVMODE: killing current process'
+                os.kill(int(pid), signal.SIGTERM)  # or SIGKILL or SIGABORT...
+        except OSError:
+            pass
+        # ok, that process is not running, so continue
+
+    else:
+        # ok, well, that's the best I can do, continue
+        pass
+
+    f = open(LOCATION + "/.raspiceilpid", 'w')
+    f.write(str(os.getpid()))
+    f.close()
+    lg.info('Beginning active data collection')
+    if devmode:
+        print "DEVMODE: no data being collected, all data printing to screen"
+
+    ser = serial.Serial()
+    ser.baudrate = BAUDRATE
+    ser.bytesize = BYTESIZE
 
 
-	ob = ''
-	while 1:
-	    time.sleep(DELAY)  # greatly reduce server load
-	    if ser.inWaiting() > 0:
-	        l = ser.read(ser.inWaiting())
-	        if devmode:
-	            print l
-	            continue
-	        ob += l
-	        # and then check if both begin and end control characters are present. if so, save the ob
-	        # WITH A TIMESTAMP!
-	        if BOM in ob and EOM in ob:
-	            save(ob)
-	            l.debug('Message received')
-	            ob = ''
-	        elif EOM in ob and not BOM in ob:
-	            # this means the recorder started in the middle of a message, save it
-	            save(ob)
-	            ob = ''
+
+    # FIXME - UTILIZE THE LIST_PORTS.COMPORTS() TO GET IDS!
+    # #ser.timeout=1
+    port = PORT
+    while port < 20:
+        # loop through all likely ports...
+        try:
+            ser.port = '/dev/ttyUSB%i' % port
+            ser.open()
+            break  # try no more!
+        except serial.serialutil.SerialException as e:
+            print e
+            port += 1
+    if port == 20:
+        l.warning('No valid USB serial port identified, aborting')
+        exit()
+
+
+    lg.info("Connected to a USB device on {}".format(ser.port))
+
+    print """
+    
+        DEVMODE: PRINTING SERIAL DATA RECEIVED. 
+        NOTE: GARBLED TEXT MEANS BAUD RATE AND/OR BYTE SIZE NOT ACCURATELY SPECIFIED 
+    
+    /////////////////////////////////////////////////////////////////////////////////
+        
+    """
+
+
+    ob = ''
+    while 1:
+        time.sleep(DELAY)  # greatly reduce server load
+        if ser.inWaiting() > 0:
+            l = ser.read(ser.inWaiting())
+            if devmode:
+                print l
+                continue
+            ob += l
+            # and then check if both begin and end control characters are present. if so, save the ob
+            # WITH A TIMESTAMP!
+            if BOM in ob and EOM in ob:
+                save(ob)
+                l.debug('Message received')
+                ob = ''
+            elif EOM in ob and not BOM in ob:
+                # this means the recorder started in the middle of a message, save it
+                save(ob)
+                ob = ''
 
 
 if __name__ == "__main__":
 
-	# use keyword 'dev' to run the code in non-recording dev/verbose mode
-	devmode = False
-	if len(sys.argv) > 1 and sys.argv[1] == 'dev':
-	    devmode = True
+    # use keyword 'dev' to run the code in non-recording dev/verbose mode
+    devmode = False
+    if len(sys.argv) > 1 and sys.argv[1] == 'dev':
+        devmode = True
 
-	main(BAUDRATE, BYTESIZE, BOM, EOM, PORT, FILESTR, LOCATION, DELAY, devmode)
+    main(BAUDRATE, BYTESIZE, BOM, EOM, PORT, FILESTR, LOCATION, DELAY, devmode)
